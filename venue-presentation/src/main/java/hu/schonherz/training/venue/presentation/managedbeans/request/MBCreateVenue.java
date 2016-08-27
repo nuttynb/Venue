fad7534e6051c36a1aba1325255f19231ceab4a8
Nagykéri Bence <nuttyka@gmail.com>
2016-08-27 15:13:16 +0200

    Added JavaEE

--- venue-presentation/src/main/java/hu/schonherz/training/venue/presentation/managedbeans/request/MBCreateVenue.java
+++ venue-presentation/src/main/java/hu/schonherz/training/venue/presentation/managedbeans/request/MBCreateVenue.java
@@ -1,6 +1,8 @@
 package hu.schonherz.training.venue.presentation.managedbeans.request;
 
 import hu.schonherz.training.venue.presentation.managedbeans.view.MBVenue;
+import hu.schonherz.training.venue.service.VenueService;
+import hu.schonherz.training.venue.service.impl.VenueServiceImpl;
 import org.primefaces.event.FlowEvent;
 
 import javax.faces.application.FacesMessage;
@@ -9,17 +11,21 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
+
 @ManagedBean(name = "createVenueBean")
 @RequestScoped
 public class MBCreateVenue {
     @ManagedProperty(value = "#{venueBean}")
     private MBVenue venue;
+    private VenueService service = new VenueServiceImpl();
 
     public String doCreate() {
-
+        //service.createVenue(venue.getVenue());
+        //VenueVo possibleVenue = service.getVenueByName(venue.getVenue().getName());
         if (false) {
             return "profile";
         } else {
+
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", venue.getVenue().getName() + " already exists.");
             FacesContext.getCurrentInstance().addMessage(null, msg);
             return null;
