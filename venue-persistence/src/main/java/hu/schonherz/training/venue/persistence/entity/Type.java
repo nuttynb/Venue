e2615ec70bcb919852a91ebfbb585e0dba976c0e
Czégényi Roland <czegenyi.roland@gmail.com>
2016-08-27 15:42:49 +0200

    Add Type repository and service.

--- venue-persistence/src/main/java/hu/schonherz/training/venue/persistence/entity/Type.java
+++ venue-persistence/src/main/java/hu/schonherz/training/venue/persistence/entity/Type.java
@@ -1,9 +1,16 @@
 package hu.schonherz.training.venue.persistence.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Home on 2016. 08. 23..
  */
+@Entity
+@Table(name = "Type")
+public class Type extends BaseEntity {
 
-public enum Type {
-    Rock_kocsma, Rom_kocsma, Disco, Bar, Night_club;
+    @Column(nullable = false)
+    private String name;
 }
---
+++ venue-service/src/main/java/hu/schonherz/training/venue/service/TypeService.java
@@ -0,0 +1,13 @@
+package hu.schonherz.training.venue.service;
+
+import hu.schonherz.training.venue.vo.TypeVo;
+import hu.schonherz.training.venue.vo.VenueVo;
+
+import java.util.List;
+
+/**
+ * Created by Roli on 2016. 08. 27..
+ */
+public interface TypeService {
+    List<TypeVo> getAllType();
+}
