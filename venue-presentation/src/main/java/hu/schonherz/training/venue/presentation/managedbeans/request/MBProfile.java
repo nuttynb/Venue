abd0e83e95aeca4e9845eb169652ddafc5bd57dd
Nagykéri Bence <nuttyka@gmail.com>
2016-08-30 04:32:14 +0200

    Initializing profile page.

---
+++ venue-presentation/src/main/java/hu/schonherz/training/venue/presentation/managedbeans/request/MBProfile.java
@@ -0,0 +1,49 @@
+package hu.schonherz.training.venue.presentation.managedbeans.request;
+
+import hu.schonherz.training.venue.presentation.managedbeans.view.MBVenue;
+import hu.schonherz.training.venue.service.VenueService;
+import hu.schonherz.training.venue.vo.VenueVo;
+import org.primefaces.event.FlowEvent;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+
+@ManagedBean(name = "profileBean")
+@RequestScoped
+public class MBProfile {
+    @ManagedProperty(value = "#{venueBean}")
+    private MBVenue venue;
+
+    @EJB
+    VenueService venueService;
+
+    public void onLoad() {
+        System.out.println("onLoad lefutott.");
+        VenueVo test = new VenueVo();
+        test.setId(new Long(1));
+        test.setName("Teszt eset");
+        test.setDescription("adatbázisból töltődik fel");
+        venue.setVenue(test);
+    }
+
+    public MBVenue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(MBVenue venue) {
+        this.venue = venue;
+    }
+
+    public VenueService getVenueService() {
+        return venueService;
+    }
+
+    public void setVenueService(VenueService venueService) {
+        this.venueService = venueService;
+    }
+}
