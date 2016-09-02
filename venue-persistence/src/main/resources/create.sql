916b2b5dd3a8a1b90c64471d0c33e56988fffcf1
Nagykéri Bence <nuttyka@gmail.com>
2016-09-02 18:06:17 +0200

    Added an example venue and address to the DB, for comfortable testing. You should use id=1 to get this, for example: http://localhost:8080/venue-presentation/?id=1&name=Teszt%20Elek

--- venue-persistence/src/main/resources/create.sql
+++ venue-persistence/src/main/resources/create.sql
@@ -9,4 +9,6 @@ INSERT INTO public.type( id, name ) VALUES (8, 'Beer garden');
 INSERT INTO public.type( id, name ) VALUES (9, 'Tavern');
 INSERT INTO public.type( id, name ) VALUES (10, 'Coffeehouse');
 INSERT INTO public.type( id, name ) VALUES (11, 'Tea house');
+INSERT INTO public.address( id, city, country, number, postcode, street ) VALUES (1, 'Debrecen', 'Magyarország', '9', 4026, 'Bethlen utca');
+INSERT INTO public.venue( id,  name, description, ownerid, address_id, type_id) VALUES(1,'Teszt Venue Név', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 1, 1, 3);
 
