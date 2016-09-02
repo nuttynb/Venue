553692a854cc06d286fb2e67d4c2dde08c794d0a
SZSalyi <salyi.szabi@gmail.com>
2016-09-02 17:02:21 +0200

    Commented version.

--- venue-persistence/src/main/java/hu/schonherz/training/venue/persistence/repository/VenueRepository.java
+++ venue-persistence/src/main/java/hu/schonherz/training/venue/persistence/repository/VenueRepository.java
@@ -1,15 +1,10 @@
 package hu.schonherz.training.venue.persistence.repository;
 
-import hu.schonherz.training.venue.persistence.entity.Type;
 import hu.schonherz.training.venue.persistence.entity.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collection;
 
 /**
  * Created by Home on 2016. 08. 25..
@@ -20,7 +15,7 @@ import java.util.Collection;
 public interface VenueRepository extends JpaRepository<Venue , Long>{
 
     Venue findVenueByOwnerId(Long ownerId);
-
+/*
     Venue findVenueById(Long Id);
 
     Venue findVeuneByAddress_Id(Long address_id);
@@ -33,5 +28,5 @@ public interface VenueRepository extends JpaRepository<Venue , Long>{
     @Query("update venue v set v.name = ?1, v.description = ?2 ,v.type_id = ?3 where v.id = ?4")
     void updateVenue(String name, String description, Long type_id, Long id) throws Exception;
 
-
+*/
 }
