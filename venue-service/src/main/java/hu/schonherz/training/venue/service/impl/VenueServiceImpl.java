5ab5cb3b90ee05f13c86d10e5f8a44c16a46379f
Nagykéri Bence <nuttyka@gmail.com>
2016-09-10 16:58:27 +0200

    Adding remote ejb service, some refactor, fixes.

--- venue-service/src/main/java/hu/schonherz/training/venue/service/impl/VenueServiceImpl.java
+++ venue-service/src/main/java/hu/schonherz/training/venue/service/impl/VenueServiceImpl.java
@@ -1,21 +1,27 @@
 package hu.schonherz.training.venue.service.impl;
 
+import hu.schonherz.training.venue.ejb.remote.stateless.VenueRemoteService;
 import hu.schonherz.training.venue.persistence.entity.Venue;
 import hu.schonherz.training.venue.persistence.repository.VenueRepository;
 import hu.schonherz.training.venue.service.VenueService;
 import hu.schonherz.training.venue.vo.VenueVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 
 import javax.ejb.*;
 import javax.interceptor.Interceptors;
+import java.util.List;
 
 @Stateless(name = "VenueService", mappedName = "VenueService")
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
 @TransactionManagement(TransactionManagementType.CONTAINER)
 @Local(VenueService.class)
+@Remote(VenueRemoteService.class)
 @Interceptors({SpringBeanAutowiringInterceptor.class})
-public class VenueServiceImpl extends AbstractMappingService implements VenueService {
+public class VenueServiceImpl extends AbstractMappingService implements VenueService, VenueRemoteService {
 
     @Autowired
     private VenueRepository venueRepository;
@@ -45,4 +51,14 @@ public class VenueServiceImpl extends AbstractMappingService implements VenueSer
         return null;
     }
 
+    @Override
+    public List<VenueVo> getAllVenues() {
+        return map(venueRepository.findAll(), VenueVo.class);
+    }
+
+    @Override
+    public List<VenueVo> getVenuesWithPaging(int page, int pageSize) {
+        Pageable pageable = new PageRequest(page, pageSize);
+        return map(venueRepository.findAll(pageable).getContent(), VenueVo.class);
+    }
 }
